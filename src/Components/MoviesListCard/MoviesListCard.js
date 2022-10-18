d5d2a6beec227776d81a6f982331254bffec7a85
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-10-18 15:27:51 +0300

    day 2

--- src/Components/MoviesListCard/MoviesListCard.js
+++ src/Components/MoviesListCard/MoviesListCard.js
@@ -2,14 +2,14 @@ import {useDispatch} from "react-redux";
 
 
 const MoviesListCard = ({movie}) => {
-    const {id, title, backdrop_path} = movie;
+    const {id, title, backdrop_path,poster_path} = movie;
 
     const dispatch = useDispatch();
 
     return (
         <div>
-            <p>{id}:{title}</p>
-            <img src={"https://image.tmdb.org/t/p/w500/" + backdrop_path} alt={backdrop_path}></img>
+            <h1>{title}</h1>
+            <img src={"https://image.tmdb.org/t/p/w500/" + poster_path} alt={title +"img"}></img>
 
         </div>
     )
